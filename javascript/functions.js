6920773a346fd427cdd551839bf9a3dc39236dbd
Zach Rickers <mzrickers@gmail.com>
2020-09-22 17:10:45 -0600

    updating the DOM

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,12 +1,44 @@
+const imgFolder = "../images/banner";
+// const fs = require('fs');
 
 // Returns true if zipcode is greater than 20000
 function zipcodeChecker(zipcode) {
   return zipcode > 20000;
 };
 
-// Tests
-// let greaterThan = zipcodeChecker(21000);
-// console.log(`True: ${greaterThan}`);
+// Can't get this to add to banner array
+// Get array of banners from folder
+// function getBannerArray(folder) {
+//   let banner = [];
+//   fs.write(folder, (err, files) => {
+//     files.forEach(file => {
+//       console.log(file);
+//       banner.push(file)   
+//     });
+//       return banner; // doesn't work not in scope for some reason.
+//   })
+// };
 
-// let lessThan = zipcodeChecker(10000);
-// console.log(`False: ${lessThan}`);
+const bannerArray = ["../images/banner.carAd.png", "../images/banner.schoolAd.png"];
+
+// selects a random ad from banners array
+function simpleBannerSelect(ads) {
+  let ad = ads[Math.floor(Math.random() * ads.length)];
+  return ad;
+}
+
+
+// Call functions
+// let testBanners = getBannerArray(imgFolder);
+// console.log(`Array: ${testBanners}`);
+
+
+let testOneBanner = simpleBannerSelect(bannerArray);
+// console.log(`One ad: ${testOneBanner}`);
+
+// Update the DOM for the banner
+
+// let element = document.getElementById("bannerImage");
+// element.setAttribute("src", testOneBanner);
+
+console.log(testOneBanner);
