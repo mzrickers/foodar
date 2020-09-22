a1d05e5c6d6d0607bfd9f1e39332a31c3cac593b
Zach Rickers <mzrickers@gmail.com>
2020-09-22 12:41:18 -0600

    updated nav bar

---
+++ javascript/functions.js
@@ -0,0 +1,12 @@
+
+// Returns true if zipcode is greater than 20000
+function zipcodeChecker(zipcode) {
+  return zipcode > 20000;
+};
+
+// Tests
+// let greaterThan = zipcodeChecker(21000);
+// console.log(`True: ${greaterThan}`);
+
+// let lessThan = zipcodeChecker(10000);
+// console.log(`False: ${lessThan}`);
